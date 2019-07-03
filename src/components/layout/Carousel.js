ed09ad74bfc4a9b96c53ead10ec7725c9ee0f251
Alain Dimabuyo <xalaindimabuyo@gmail.com>
2019-07-04 03:10:08 +0800

    fix slide

--- src/components/layout/Carousel.js
+++ src/components/layout/Carousel.js
@@ -26,77 +26,21 @@ const CoverflowEffect = ({ carousel }) => {
 
   return (
     <Swiper {...params}>
-      {carousel.x_0_type === "image" ? (
-        <div className='swiper-slide'>
-          <img src={carousel.x_0_images_standard_resolution_url} alt='' />
+      <div className='container'>
+        <div className='row'>
+          <div className='col s6'>
+            <div className='swiper-slide'>
+              <img src={carousel.x_0_images_standard_resolution_url} alt='' />
+            </div>
+            <div className='swiper-slide'>
+              <img src={carousel.x_1_images_standard_resolution_url} alt='' />
+            </div>
+            <div className='swiper-slide'>
+              <img src={carousel.x_2_images_standard_resolution_url} alt='' />
+            </div>
+          </div>
         </div>
-      ) : (
-        false
-      )}
-      {carousel.x_1_type === "image" ? (
-        <div className='swiper-slide'>
-          <img src={carousel.x_1_images_standard_resolution_url} alt='' />
-        </div>
-      ) : (
-        false
-      )}
-      {carousel.x_2_type === "image" ? (
-        <div className='swiper-slide'>
-          <img src={carousel.x_2_images_standard_resolution_url} alt='' />
-        </div>
-      ) : (
-        <div className='swiper-slide noSwiping	'>
-          <img src={carousel.x_0_images_standard_resolution_url} alt='' />
-        </div>
-      )}
-      {carousel.x_3_type === "image" ? (
-        <div className='swiper-slide'>
-          <img src={carousel.x_3_images_standard_resolution_url} alt='' />
-        </div>
-      ) : (
-        <div className='swiper-slide noSwiping	'>
-          <img src={carousel.x_0_images_standard_resolution_url} alt='' />
-        </div>
-      )}
-      {carousel.x_4_type === "image" ? (
-        <div className='swiper-slide'>
-          <img src={carousel.x_4_images_standard_resolution_url} alt='' />
-        </div>
-      ) : (
-        <div className='swiper-slide noSwiping	'>
-          <img src={carousel.x_1_images_standard_resolution_url} alt='' />
-        </div>
-      )}
-
-      {/* 
-
-      <div className='swiper-slide'>
-        <img src={carousel.x_0_images_standard_resolution_url} alt='' />
       </div>
-      <div className='swiper-slide'>
-        <img src={carousel.x_1_images_standard_resolution_url} alt='' />
-      </div>
-      <div className='swiper-slide'>
-        <img src={carousel.x_2_images_standard_resolution_url} alt='' />
-      </div>
-      <div className='swiper-slide'>
-        <img src={carousel.x_3_images_standard_resolution_url} alt='' />
-      </div>
-      <div className='swiper-slide'>
-        <img src={carousel.x_4_images_standard_resolution_url} alt='' />
-      </div>
-      <div className='swiper-slide'>
-        <img src={carousel.x_5_images_standard_resolution_url} alt='' />
-      </div>
-      <div className='swiper-slide'>
-        <img src={carousel.x_6_images_standard_resolution_url} alt='' />
-      </div>
-      <div className='swiper-slide'>
-        <img src={carousel.x_7_images_standard_resolution_url} alt='' />
-      </div>
-      <div className='swiper-slide'>
-        <img src={carousel.x_8_images_standard_resolution_url} alt='' />
-      </div> */}
     </Swiper>
   );
 };
